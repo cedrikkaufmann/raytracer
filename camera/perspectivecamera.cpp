2cec10a92b237bc525ccf601400a6a4f6ae7b9a1
ckaufmann <cedrik.kaufmann@gmail.com>
2016-11-07 13:04:50 +0100

    G40

--- camera/perspectivecamera.cpp
+++ camera/perspectivecamera.cpp
@@ -12,5 +12,19 @@ Ray PerspectiveCamera::castRay(float x, float y) const {
   * Finally, create a new ray, and set its origin and direction.
   *
   */
+
+  double window_h = tan(fovAngle_/2) * length(forwardDirection_);
+  double window_w = window_h;
+
+  double norm_x = x / window_h;
+  double norm_y = y / window_w;
+
+  Ray ray;
+  ray.origin = position;
+  Vector3d focus = forwardDirection_ * length(forwardDirection_);
+
+  ray.direction = Vector3d(norm_x, norm_y, INFINITY)
+
+
   return Ray();
 }
