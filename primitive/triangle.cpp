47b1028af0000f7e3df9a44772ff5dfcd03de6f5
ckaufmann <cedrik.kaufmann@gmail.com>
2016-11-08 17:10:03 +0100

    Add implementation of triangle intersection

--- primitive/triangle.cpp
+++ primitive/triangle.cpp
@@ -35,7 +35,58 @@ bool Triangle::intersect(Ray * ray) const {
   * Also return true/false, whether the primitive was hit or not respectively.
   *
   */
-  return false;
+
+  // Span plane defined by vertices of triangle
+  // Using a as base vector and edges between a,b and a,c
+  Vector3d base = vertex_[0];
+  Vector3d edge_b = vertex_[1] - vertex_[0];
+  Vector3d edge_c = vertex_[2] - vertex_[0];
+
+  // Calculating normal vector of plane
+  Vector3d normal = crossProduct(edge_b, edge_c);
+  normalize(&normal);
+
+  // Calculation of intersection using hesse normal form
+  if (float equation_denominator = dotProduct(ray->direction, normal) != 0.0) {
+      float normal_distance = dotProduct(ray->direction, normal);
+      float equation_numerator = normal_distance - dotProduct(ray->origin, normal);
+
+      if (float intersection = equation_numerator / equation_denominator <= 0.0) {
+          // Behind visible area, parallel or in plane
+          return false;
+      } else {
+          // Intersection with plane, check if also with triangle
+
+          // Calculate intersection
+          Vector3d intersection_plane = ray->origin + normal_distance * ray->direction;
+          Vector3d edge_a = intersection_plane - base;
+
+          // Calculate barycentric coordinates
+          float barycentric_denominator = dotProduct(edge_c, edge_c) * dotProduct(edge_b, edge_b) - dotProduct(edge_c, edge_b) * dotProduct(edge_b, edge_c);
+
+          float barycentric_a = (dotProduct(edge_a, edge_b) * dotProduct(edge_c, edge_c) - dotProduct(edge_a, edge_c) * dotProduct(edge_c, edge_b))
+                  / barycentric_denominator;
+          float barycentric_b = (dotProduct(edge_a, edge_c) * dotProduct(edge_b, edge_b) - dotProduct(edge_a, edge_b) * dotProduct(edge_b, edge_c))
+                  / barycentric_denominator;
+
+          // Use Barycentric Coordinates to determine intersection
+          if (barycentric_a < 0 || barycentric_a > 1) {
+              return false;
+          } else if (barycentric_b < 0) {
+              return false;
+          } else if (barycentric_a + barycentric_b > 1) {
+              return false;
+          } else {
+              ray->length = normal_distance;
+              ray->primitive = this;
+
+              return true;
+          }
+      }
+  } else {
+      // No solution divide by zero
+      return false;
+  }
 }
 
 Vector3d Triangle::normalFromRay(Ray const& ray) const {
