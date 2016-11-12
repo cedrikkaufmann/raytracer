43e34d1daf67691da61ec97ae9bbd81c77a1172b
ckaufmann <cedrik.kaufmann@gmail.com>
2016-11-12 14:18:06 +0100

    Fix perspectivcamera

--- camera/perspectivecamera.cpp
+++ camera/perspectivecamera.cpp
@@ -4,14 +4,18 @@ PerspectiveCamera::PerspectiveCamera()
   : forwardDirection_(0,0,-1), upDirection_(0,1,0), fovAngle_(70) {}
 
 Ray PerspectiveCamera::castRay(float x, float y) const {
-  Vector3d direction(x * tan(fovAngle_/2 * M_PI/180),
-                     y * tan(fovAngle_/2 * M_PI/180),
-                     forwardDirection_.z);
-  normalize(&direction);
+  // Set up the coordinate system
+  Vector3d const zAxis = normalized(this->forwardDirection_);
+  Vector3d const xAxis = normalized(crossProduct(zAxis, this->upDirection_));
+  Vector3d const yAxis = normalized(crossProduct(xAxis, zAxis));
 
+  // Calculate the focus
+  float const focus = 1.0 / std::tan((this->fovAngle_*PI/180) / 2);
+
+  // Create a ray
   Ray ray;
-  ray.origin = position_;
-  ray.direction = direction;
-
+  ray.origin = this->position_;
+  ray.direction = x*xAxis + y*yAxis + focus*zAxis;
+  normalize(&ray.direction);
   return ray;
 }
