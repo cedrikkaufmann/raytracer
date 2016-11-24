f633e029afebe2c6d52b7c9bc921f7f1f5514b4a
Jana Rehbein <Jana Rehbein>
2016-11-24 17:24:16 +0100

    grundgerüst klasse geschrieben

--- light/ambientlight.cpp
+++ light/ambientlight.cpp
@@ -1,5 +1,12 @@
-/*
-* IMPLEMENT ME!
-*
-*
-*/
+#include "scene/scene.h"
+#include "ambientlight.h"
+#include "primitive/primitive.h"
+
+AmbientLight::AmbientLight() {}
+
+AmbientLight::AmbientLight(float intensity, const Color &color)
+    : Light(intensity, color), position(position) {}
+
+Light::Illumination AmbientLight::illuminate(Ray const& ray) {
+
+}
