30ef29b743066078567e5d6c7f613817a4bdf55e
ckaufmann <cedrik.kaufmann@gmail.com>
2016-12-12 17:02:57 +0100

    Add downloader script for brdf binary files

---
+++ binary-downloader.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -f *.binary
+wget http://people.csail.mit.edu/wojciech/BRDFDatabase/brdfs/green-acrylic.binary
+wget http://people.csail.mit.edu/wojciech/BRDFDatabase/brdfs/gold-paint.binary
