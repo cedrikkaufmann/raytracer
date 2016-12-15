75e8e7160313bb8cc7fa47022572bc144b94e50d
ckaufmann <cedrik.kaufmann@gmail.com>
2016-12-15 11:51:03 +0100

    Update

--- primitive/texturedtriangle.cpp
+++ primitive/texturedtriangle.cpp
@@ -1,24 +1,22 @@
-#include "primitive/smoothtriangle.h"
+#include "primitive/texturedtriangle.h"
 
 
 // Constructor /////////////////////////////////////////////////////////////////
 
-SmoothTriangle::SmoothTriangle(Shader * shader)
-  : Triangle(shader) {}
+TexturedTriangle::TexturedTriangle(Shader * shader)
+  : SmoothTriangle(shader) {}
 
-SmoothTriangle::SmoothTriangle(Vector3d const& a, Vector3d const& b, Vector3d const& c,
+TexturedTriangle::TexturedTriangle(Vector3d const& a, Vector3d const& b, Vector3d const& c,
                                Vector3d const& na, Vector3d const& nb, Vector3d const& nc,
                                Vector2d const& ta, Vector2d const& tb, Vector2d const& tc,
                                Shader * shader)
-  : Triangle(a,b,c,shader), normal_{na,nb,nc}, textureCoordinates_{ta, tb, tc} {}
+  : SmoothTriangle(a,b,c,na,nb,nc,shader), textureCoordinates_{ta, tb, tc} {}
 
 
 // Primitive functions /////////////////////////////////////////////////////////
 
-Vector3d SmoothTriangle::normalFromRay(Ray const& ray) const {
+Vector2d TexturedTriangle::uvFromRay(Ray const& ray) const {
   // Implement me!
-  // Use Triangle::uvFromRay to get the UV coordinates.
-  Vector3d normal = ray.surfacePosition.u * normal_[1] + ray.surfacePosition.v * normal_[2] + (1 - ray.surfacePosition.u - ray.surfacePosition.v) * normal_[0];
-  normalize(&normal);
+  Vector2d normal(0,0);
   return normal;
 }
