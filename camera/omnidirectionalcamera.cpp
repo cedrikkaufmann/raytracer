eecf76587186a99390bf747321daf1a82d143274
Daniel Szafranski <y0072613@ibr.cs.tu-bs.de>
2016-12-15 11:33:30 +0100

    update

--- camera/omnidirectionalcamera.cpp
+++ camera/omnidirectionalcamera.cpp
@@ -2,7 +2,21 @@
 
 Ray OmnidirectionalCamera::castRay(float x, float y) const {
 
-  // IMPLEMENT ME!
+    // Calculate the focus
 
-  return Ray();
+    float phi = 2 * PI * x;
+    float theta = 1 * PI * y;
+
+    float r = 1;
+    float x_1 = r * sin(theta) * cos(phi);
+    float y_1 = r * sin(theta) * sin(phi);
+    float z_1 = r * cos(theta);
+
+
+    // Create a ray
+    Ray ray;
+    ray.origin = this->position_;
+    ray.direction = Vector3d(x_1,y_1,z_1);
+    normalize(&ray.direction);
+    return ray;
 }
