bdd507ac320f2caf4232e7de2a7312301949ee64
ckaufmann <cedrik.kaufmann@gmail.com>
2016-12-15 11:46:23 +0100

    Add Texturedtrinagle class

--- primitive/texturedtriangle.cpp
+++ primitive/texturedtriangle.cpp
@@ -1,1 +1,24 @@
-// IMPLEMENT ME!
+#include "primitive/smoothtriangle.h"
+
+
+// Constructor /////////////////////////////////////////////////////////////////
+
+SmoothTriangle::SmoothTriangle(Shader * shader)
+  : Triangle(shader) {}
+
+SmoothTriangle::SmoothTriangle(Vector3d const& a, Vector3d const& b, Vector3d const& c,
+                               Vector3d const& na, Vector3d const& nb, Vector3d const& nc,
+                               Vector2d const& ta, Vector2d const& tb, Vector2d const& tc,
+                               Shader * shader)
+  : Triangle(a,b,c,shader), normal_{na,nb,nc}, textureCoordinates_{ta, tb, tc} {}
+
+
+// Primitive functions /////////////////////////////////////////////////////////
+
+Vector3d SmoothTriangle::normalFromRay(Ray const& ray) const {
+  // Implement me!
+  // Use Triangle::uvFromRay to get the UV coordinates.
+  Vector3d normal = ray.surfacePosition.u * normal_[1] + ray.surfacePosition.v * normal_[2] + (1 - ray.surfacePosition.u - ray.surfacePosition.v) * normal_[0];
+  normalize(&normal);
+  return normal;
+}
