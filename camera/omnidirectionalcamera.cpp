b296afb0eb09ef40f86da62e3b59a5335dcd09be
ckaufmann <cedrik.kaufmann@gmail.com>
2016-12-15 11:36:05 +0100

    Update very simple calculation

--- camera/omnidirectionalcamera.cpp
+++ camera/omnidirectionalcamera.cpp
@@ -1,8 +1,21 @@
 #include "camera/omnidirectionalcamera.h"
 
 Ray OmnidirectionalCamera::castRay(float x, float y) const {
+    // Simply calculate angle
+    float const phi = 2 * PI * x;
+    float const theta = PI * y;
 
-  // IMPLEMENT ME!
+    // Simply calculate cartesian coordinates from previous calculated angles
+    float const u = sin(theta) * cos(phi);
+    float const v = sin(theta) * sin(phi);
+    float const z = cos(theta);
+
+    // Create a ray
+    Ray ray;
+    ray.origin = this->position_;
+    ray.direction = Vector3d(u, v, z);
+    normalize(&ray.direction);
+    return ray;
 
   return Ray();
 }
